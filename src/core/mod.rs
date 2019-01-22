0672e4dcd0d18a9a618e3a97729795d224ea5b8c
nikos <nikos@mugsoft.io>
2019-01-23 03:13:47 +0400

    input cards from a json file

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,2 +1,2 @@
 pub mod card_generator;
-mod types;
+pub mod types;
--- src/flags.rs
+++ src/flags.rs
@@ -1,21 +1,52 @@
+use clap::{
+    App,
+    Arg,
+};
 pub fn handle_flags()
 {
-    let options = clap::App::new("MugSoft Tombala")
+    let options = App::new("MugSoft Tombala")
         .arg(
-            clap::Arg::with_name("gen_cards")
+            Arg::with_name("gen_cards")
                 .short("-g")
                 .long("--gen-cards")
+                .takes_value(false)
                 .help("generate random cards"),
+        )
+        .arg(
+            Arg::with_name("cards")
+                .short("-c")
+                .long("--cards")
+                .required_unless("gen_cards")
+                .help("cards json")
+                .default_value("./tombala_cards.json")
+                .takes_value(true),
         )
         .get_matches();
     if options.is_present("gen_cards")
     {
         handle_g_flag();
     }
+    if let Some(path) = options.value_of("cards")
+    {
+        handle_c_flag(path);
+    }
 }
-
-fn handle_g_flag()
+fn handle_c_flag(path: &str) //{{{
+{
+    let cards = std::fs::read_to_string(path);
+    if let Ok(cards) = cards
+    {
+        let cards = serde_json::from_str::<Vec<super::core::types::Card>>(&cards);
+        println!("{:?}", cards.unwrap());
+    }
+    else if let Err(e) = cards
+    {
+        eprintln!("Cannot read the cards {}", e);
+        std::process::exit(e.raw_os_error().unwrap_or(1));
+    }
+} //}}}
+fn handle_g_flag() //{{{
 {
     let cards = crate::core::card_generator::generate_n_cards(135);
     let _ = std::fs::write("tombala_cards.json", serde_json::to_string(&cards).unwrap());
-}
+} //}}}
