e03d463ff8373041746271702f453eb9978afdd6
nikos <nikos@mugsoft.io>
2019-02-09 18:51:38 +0400

    add data inside new game and args

--- src/command_parser.rs
+++ src/command_parser.rs
@@ -1,75 +1,104 @@
 ///handle_command returns a response and whether the connection should be terminated
-pub fn parse_command(cmd: Command) -> COMMAND
+fn parse_command(cmd: Command) -> COMMAND
 {
     use COMMAND::*;
     if let Some(err) = cmd.error
     {
         return Error(err);
     }
-    match cmd.cmd
+    match cmd.cmd.as_str()
     {
-        "new_game" => NewGame,
-        "new_ball" =>
-        //{{{
+        "new_game" =>
         {
-            if cmd.args.len() < 1 || cmd.args[0] > 90 || cmd.args[0] < 1
+            if cmd.args.is_none()
             {
-                return Error("invalid ball number");
+                NewGame(None)
             }
-            NewBall(cmd.args[0])
-        } //}}}
-        _ => Error("unknown command"),
+            else
+            {
+                NewGame(Some(cmd.args.unwrap()[0]))
+            }
+        }
+        "new_ball" =>
+        {
+            let args = if let Some(args) = cmd.args
+            {
+                args
+            }
+            else
+            {
+                return Error("invalid ball number".to_string());
+            };
+            if args.len() < 1 || args[0] > 90 || args[0] < 1
+            {
+                return Error("invalid ball number".to_string());
+            }
+            NewBall(args[0])
+        }
+        _ => Error("unknown command".to_string()),
     }
-} //}}}
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum COMMAND
 {
-    NewGame,
+    NewGame(Option<u32>),
     NewBall(u32),
     Winning(Vec<u8>),
-    Error(&'static str),
+    Error(String),
 }
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Default, Serialize, Deserialize, Debug, Clone)]
 pub struct Command
 {
-    cmd: &'static str,
-    args: Vec<u32>,
-    error: Option<&'static str>,
+    pub cmd: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub args: Option<Vec<u32>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error: Option<String>,
 }
+
+impl std::convert::From<Command> for COMMAND
+{
+    fn from(c: Command) -> COMMAND
+    {
+        if c.error.is_some()
+        {
+            return COMMAND::Error(c.error.unwrap());
+        }
+        parse_command(c)
+    }
+}
+
 #[cfg(test)]
 mod tests
 {
     use super::*;
     #[test]
     fn test_command_parser()
-    //{{{
     {
         use COMMAND::*;
-        //{{{cases
         let cmds = vec![
             (
                 Command {
-                    cmd: "new_game",
-                    args: vec![],
+                    cmd: "new_game".to_string(),
+                    args: None,
                     error: None,
                 },
                 NewGame,
             ),
             (
                 Command {
-                    cmd: "new_ball",
-                    args: vec![12],
+                    cmd: "new_ball".to_string(),
+                    args: Some(vec![12]),
                     error: None,
                 },
                 NewBall(12),
             ),
         ];
-        //}}}
         for (input, expected) in cmds
         {
             let actual = parse_command(input);
             assert_eq!(actual, expected)
         }
-    } //}}}
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate rand;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+pub mod command_parser;
 pub mod core;
 pub mod delivery;
 pub mod flags;
