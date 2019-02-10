838c77b7f8e457c8d2efd0ebb9ef2d652b829b99
nikos <nikos@mugsoft.io>
2019-02-10 06:28:35 +0400

    use domains socket

--- src/delivery/socket.rs
+++ src/delivery/socket.rs
@@ -1,17 +1,34 @@
-use std::io::Read;
-use std::net::TcpListener;
+#![allow(unused_must_use)]
+use crate::command_parser::{
+    Command,
+    COMMAND,
+};
+use std::error::Error;
+use std::io::{
+    Read,
+    Write,
+};
+use std::os::unix::net::{
+    UnixListener,
+    UnixStream,
+};
+use std::rc::Rc;
 use std::sync::mpsc::{
     Receiver,
     SyncSender,
 };
-use std::sync::Arc;
-pub fn start_server(receiver: SyncSender<String>, server_sent_command: Receiver<String>)
+pub fn start_server(
+    cmd: SyncSender<COMMAND>,
+    msg: Receiver<(COMMAND, bool, Option<crate::core::types::Winning>)>,
+)
 {
-    let receiver = Arc::new(receiver);
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    std::fs::remove_file("/tmp/tombala.sock");
+    let listener = UnixListener::bind("/tmp/tombala.sock").unwrap();
+    let cmd = Rc::new(cmd);
+    let msg = Rc::new(msg);
     loop
     {
-        let mut s = match listener.accept()
+        let s = match listener.accept()
         {
             Ok((s, _)) => s,
             Err(e) =>
@@ -20,18 +37,97 @@ pub fn start_server(receiver: SyncSender<String>, server_sent_command: Receiver<
                 continue;
             }
         };
-        let receiver = receiver.clone();
-        let mut data: [u8; 10] = [0; 10];
-        loop
+        //handle only on econnection at a given time
+        handle_connection(s, cmd.clone(), msg.clone());
+    }
+}
+
+fn handle_connection(
+    c: UnixStream,
+    command: Rc<SyncSender<COMMAND>>,
+    msg: Rc<Receiver<(COMMAND, bool, Option<crate::core::types::Winning>)>>,
+)
+{
+    const MAX_DATA_LEN: usize = 1024; //max data size in bytes
+    let mut c = c;
+    loop
+    // for every command
+    {
+        let mut buffer = [0; MAX_DATA_LEN];
+        let read = c.read(&mut buffer);
+        let num_read = if let Ok(n) = read { n } else { break };
+        if num_read == 0
         {
-            let num = s.read(&mut data).unwrap();
-            if num == 0
+            break;
+        }
+        // let cmd: Result<Command, serde_json::Error> = serde_json::from_reader(c);
+        let str_val = String::from_utf8_lossy(&buffer[..num_read]).into_owned();
+        let val = str_val.trim_end().to_string();
+        let cmd = serde_json::from_str(val.as_str());
+        // check if command is valid
+        let cmd: Command = if let Ok(cmd) = cmd
+        {
+            cmd
+        }
+        else
+        {
+            serde_json::to_writer(
+                c,
+                &Command {
+                    error: Some(String::from("invalid cmd json")),
+                    ..Default::default()
+                },
+            );
+            break;
+        };
+
+        if let Err(e) = command.send(cmd.into())
+        // send command to the actual game
+        {
+            serde_json::to_writer(
+                c,
+                &Command {
+                    error: Some(format!("something went wrong: {}", e.description())),
+                    ..Default::default()
+                },
+            );
+            break;
+        };
+
+        match msg.recv() // wait reply from the game
+        {
+            Ok((cmd, terminate,winning)) =>
             {
+                let mut cmd:Command =cmd.into() ;
+                cmd.winning=winning;
+                 serde_json::to_string(&cmd).and_then(|val|{
+					 c.write_all(val.as_bytes());
+					 Ok(())
+				});
+                if terminate
+                {
+                    break;
+                }
+            }
+            Err(e) =>
+            {
+                send_error(c, e);
                 break;
             }
-            receiver
-                .send(String::from_utf8_lossy(&data[..num]).into())
-                .expect("error");
         }
     }
+}
+
+fn send_error<T, E>(writer: T, err: E)
+where
+    T: std::io::Write,
+    E: Error,
+{
+    serde_json::to_writer(
+        writer,
+        &Command {
+            error: Some(err.description().to_string()),
+            ..Default::default()
+        },
+    );
 }
