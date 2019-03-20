67a53d67f4adef9f61cbeba34f027a57b69b1e3b
nikos <nikos@mugsoft.io>
2019-03-20 05:16:41 +0400

    fix stupid errors

--- src/testclient.rs
+++ src/testclient.rs
@@ -1,17 +1,20 @@
-use std::io::{
-    Read,
-    Write,
+use std::{
+    io::{
+        Read,
+        Write,
+    },
+    os::unix::net::UnixStream,
 };
-use std::os::unix::net::UnixStream;
 fn main()
 {
     let mut conn = UnixStream::connect("/tmp/tombala.sock").unwrap();
     for i in 1..91
     {
-        write!(conn, r#"{{"cmd":"new_ball","args":[{}]}}"#, i);
+        write!(conn, r#"{{"cmd":"new_ball","args":[{}]}}"#, i).unwrap();
         let mut data = [0; 100];
         let n = conn.read(&mut data);
         println!("{}", String::from_utf8_lossy(&data[..n.unwrap()]));
+        #[allow(deprecated)]
         std::thread::sleep_ms(100);
     }
 }
