30c8999db8a5f5ea06d31999a78ddefa3eee8201
nikos efthias <nikos@mugsoft.io>
2020-05-03 04:44:09 +0400

    Squashed commit of the following:
    commit 6931a7b3e3e59148a159d907e6f9d16c3a3c52a4
    Author: nikos efthias <nikos@mugsoft.io>
    Date:   Sun May 3 04:42:03 2020 +0400
    
        extra bonuses c1 first5 and c2 first 10
    
    commit 0a802fe66a195a3dedf7b292bf034364a21e031b
    Author: nikos efthias <nikos@mugsoft.io>
    Date:   Sun May 3 04:24:33 2020 +0400
    
        c2 on 10 balls
    
    commit aba354968d5565118d7cbe3e267c7e48b71a555d
    Author: nikos efthias <nikos@mugsoft.io>
    Date:   Sun May 3 04:23:41 2020 +0400
    
        add c1 on first 5 balls bonus
    
    commit 4505f59aa5211b5dff46435ddf66b71f604d3eba
    Author: nikos efthias <nikos@mugsoft.io>
    Date:   Sun May 3 04:21:34 2020 +0400
    
        reformat

--- src/types.rs
+++ src/types.rs
@@ -1,28 +1,17 @@
-use serde::{
-    Deserialize,
-    Serialize,
-};
+use serde::{Deserialize, Serialize};
 use std::{
-    collections::{
-        HashMap,
-        HashSet,
-    },
-    sync::{
-        Arc,
-        RwLock,
-    },
+    collections::{HashMap, HashSet},
+    sync::{Arc, RwLock},
 };
 #[derive(Deserialize, Serialize, Clone, Debug)]
-pub enum Winning
-{
+pub enum Winning {
     C1(Vec<u32>),
     C2(Vec<u32>),
     T(Vec<u32>),
-    TULUM(Vec<u32>)
+    TULUM(Vec<u32>),
 }
 #[derive(Default, Debug, Clone)]
-pub struct Game
-{
+pub struct Game {
     pub id: u64,
     pub c1: Vec<u32>,
     pub c2: Vec<u32>,
@@ -31,25 +20,26 @@ pub struct Game
     pub card_state: HashMap<u32, CardState>,
     pub card_index_by_number: CardIndex,
 }
-impl Game
-{
-    pub fn new(game_id: u64, index: CardIndex) -> Arc<RwLock<Game>>
-    {
+impl Game {
+    pub fn new(game_id: u64, index: CardIndex) -> Arc<RwLock<Game>> {
         Arc::new(RwLock::new(Game {
             id: game_id,
             card_index_by_number: index,
             ..Default::default()
         }))
     }
-
-    pub fn is_tulum(&self)->bool
-    {
-        let (c1,c2,c3)=(&self.c1,&self.c2,&self.t);
-        c1.len()==1&&c2.len()==1&&c3.len()==1&&c1[0]==c2[0]&&c2[0]==c3[0]
+    pub fn is_first5_c1(&self) -> bool {
+        self.c1.len() > 0 && self.balls.len() == 5
+    }
+    pub fn is_first10_c2(&self) -> bool {
+        self.c2.len() > 0 && self.balls.len() == 10
+    }
+    pub fn is_tulum(&self) -> bool {
+        let (c1, c2, c3) = (&self.c1, &self.c2, &self.t);
+        c1.len() == 1 && c2.len() == 1 && c3.len() == 1 && c1[0] == c2[0] && c2[0] == c3[0]
     }
 
-    pub fn new_game(&mut self) -> u64
-    {
+    pub fn new_game(&mut self) -> u64 {
         self.id += 1;
         self.balls = Vec::new();
         self.c1 = Vec::new();
@@ -61,87 +51,63 @@ impl Game
 
     pub fn add_ball(&mut self, num: u8) -> Result<Option<Winning>, String> //{{{
     {
-        if self.t.len() > 0
-        {
+        if self.t.len() > 0 {
             return Err("game finished".to_string());
-        }
-        else if self.balls.contains(&num)
-        {
+        } else if self.balls.contains(&num) {
             return Err("ball exists".to_string());
         }
         self.balls.push(num);
-        let val = match self.card_index_by_number.get(&num)
-        {
+        let val = match self.card_index_by_number.get(&num) {
             Some(val) => val,
             None => return Ok(None),
         };
         let mut winner_cards: HashSet<u32> = HashSet::new();
-        let num_row_required_to_win = if self.c1.len() == 0
-        {
+        let num_row_required_to_win = if self.c1.len() == 0 {
             0
-        }
-        else if self.c2.len() == 0
-        {
+        } else if self.c2.len() == 0 {
             1
-        }
-        else if self.t.len() == 0
-        {
+        } else if self.t.len() == 0 {
             2
-        }
-        else
-        {
+        } else {
             return Ok(Some(Winning::T(self.t.clone())));
         };
-        for card_info in val.iter()
-        {
+        for card_info in val.iter() {
             let card_state = self.card_state.entry(card_info.card_id).or_default();
             let mut num_rows_completed = 0;
-            match card_info.row
-            {
+            match card_info.row {
                 Row::R1 => card_state.r1 += 1,
                 Row::R2 => card_state.r2 += 1,
                 Row::R3 => card_state.r3 += 1,
             };
-            if card_state.r1 > 4
-            {
+            if card_state.r1 > 4 {
                 num_rows_completed += 1;
             }
-            if card_state.r2 > 4
-            {
+            if card_state.r2 > 4 {
                 num_rows_completed += 1;
             }
-            if card_state.r3 > 4
-            {
+            if card_state.r3 > 4 {
                 num_rows_completed += 1;
             }
-            if num_rows_completed > num_row_required_to_win
-            {
+            if num_rows_completed > num_row_required_to_win {
                 winner_cards.insert(card_info.card_id);
             }
         }
-        if !winner_cards.is_empty()
-        {
-            Ok(Some(match num_row_required_to_win
-            {
-                0 =>
-                {
+        if !winner_cards.is_empty() {
+            Ok(Some(match num_row_required_to_win {
+                0 => {
                     self.c1 = winner_cards.into_iter().collect();
                     Winning::C1(self.c1.clone())
                 }
-                1 =>
-                {
+                1 => {
                     self.c2 = winner_cards.into_iter().collect();
                     Winning::C2(self.c2.clone())
                 }
-                _ =>
-                {
+                _ => {
                     self.t = winner_cards.into_iter().collect();
                     Winning::T(self.t.clone())
                 }
             }))
-        }
-        else
-        {
+        } else {
             Ok(None)
         }
     }
@@ -150,21 +116,18 @@ impl Game
 }
 pub type CardIndex = HashMap<u8, Vec<Cardinfo>>; //number to card info
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum Row
-{
+pub enum Row {
     R1,
     R2,
     R3,
 }
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Cardinfo
-{
+pub struct Cardinfo {
     pub card_id: u32,
     pub row: Row,
 }
 #[derive(Default, Debug, Serialize, Deserialize, Clone)]
-pub struct CardState
-{
+pub struct CardState {
     pub r1: u8,
     pub r2: u8,
     pub r3: u8,
